8a6d81e79428f32a696d02fd26a239f1da6de363
mrmojirising <fknmad@ukr.net>
2026-01-30 23:44:25 +0100

    add grab

--- Player/player.gd
+++ Player/player.gd
@@ -1,9 +1,53 @@
+class_name Player
 extends CharacterBody2D
 
+@export var drop_point : Marker2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var _interactables : Array[Interactable]
+var _hovered_interactalbe : Interactable
+var _picked_interactable : Interactable
+
+func add_interactable(obj : Interactable) -> void:
+	_interactables.push_back(obj)
+	if _interactables.size() == 1:
+		_hovered_interactalbe = _interactables[0]
+		_hovered_interactalbe.on_hover()
+
+func remove_interactable(obj : Interactable) -> void:
+	_interactables.erase(obj)
+	if _interactables.is_empty():
+		_hovered_interactalbe.on_unhover()
+		_hovered_interactalbe = null
+
+func _process(delta: float) -> void:
+	_update_interactables()
+	_move_picked()
+
+func _move_picked() -> void:
+	if !_picked_interactable:
+		return
+	
+	_picked_interactable.position = drop_point.global_position
+		
+func _update_interactables() -> void:
+	if _interactables.is_empty() or _picked_interactable:
+		return
+	
+	var distance = position.distance_to(_hovered_interactalbe.position)
+	var closest = _hovered_interactalbe
+	for i in _interactables:
+		var d = position.distance_to(i.position)
+		if d < distance:
+			distance = d
+			closest = i
+	
+	if closest != _hovered_interactalbe:
+		_hovered_interactalbe.on_unhover()
+		_hovered_interactalbe = closest
+		_hovered_interactalbe.on_hover()
 
 func _physics_process(delta: float) -> void:
 	var direction : Vector2 = Vector2.ZERO
@@ -14,15 +58,21 @@ func _physics_process(delta: float) -> void:
 	velocity = SPEED * direction
 
 	var collision = move_and_collide(velocity * delta)
-	#move_and_slide()
 	if collision:
-		#for c in collisions:
-		#for index in get_slide_collision_count():
-			#var collision = get_slide_collision(index)
 			var other = collision.get_collider()
-			
-			if other is Interactable:
+			if other is Interactable and !other.is_picked:
 				var push_dir = collision.get_normal()
 				var push_force : float = 100.0
 				other.add_push(push_force, -push_dir)
-			
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("Interact"):
+		if _picked_interactable:
+			_picked_interactable.on_drop()
+			_picked_interactable = null
+			if _interactables.size() == 1:
+				_hovered_interactalbe = _interactables[0]
+				_hovered_interactalbe.on_hover()
+		elif _hovered_interactalbe:
+			_hovered_interactalbe.on_interact()
+			_picked_interactable = _hovered_interactalbe
--- World/Interactables/interactable.gd
+++ World/Interactables/interactable.gd
@@ -1,10 +1,15 @@
 class_name Interactable
 extends RigidBody2D
 
-@export var push_force : float = 20.0
+@export var push_force : float = 15.0
 @export var mu_static = 0.8  # friction coefficients
 @export var mu_moving = 0.5  # pushing something moving is easier
-@export var weight : float = 8000.0
+@export var weight : float = 16000.0
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+
+var is_picked := false
 
 func add_push(force: float, direction : Vector2) -> void:
 	apply_impulse(direction * push_force * force)
@@ -14,3 +19,32 @@ func _physics_process(delta: float) -> void:
 		apply_force(- self.weight * mu_static * self.linear_velocity.normalized())
 	else:
 		apply_force(- self.weight * mu_moving * self.linear_velocity.normalized())
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Player:
+		body.add_interactable(self)
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body is Player:
+		body.remove_interactable(self)
+
+func on_hover() -> void:
+	sprite_2d.modulate = Color.RED
+
+func on_unhover() -> void:
+	sprite_2d.modulate = Color.WHITE
+
+func on_interact() -> void:
+	sprite_2d.modulate = Color.WHITE
+	is_picked = true
+	freeze = true
+	sleeping = true
+	collision_shape_2d.disabled = true
+
+func on_drop() -> void:
+	is_picked = false
+	freeze = false
+	sleeping = false
+	collision_shape_2d.disabled = false
