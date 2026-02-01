4277a4299e71bc599f4244d9d751befe3f191570
Saillou <chauvinremy@upyourprod.fr>
2026-02-01 03:05:18 +0100

    Add end results

--- Fight/Scripts/Fight.gd
+++ Fight/Scripts/Fight.gd
@@ -1,8 +1,9 @@
-extends Node2D
+class_name FightManager extends Node2D
 
-const FIGHTER_SCENE := preload("res://Fight/Fighter/Fighter.tscn")
+# Public API
+enum Result { Unknown, Win, Lose }
 
-var _i_current_spawn:int = 0
+signal FightEnd(result:Result)
 
 const op_chars = [
 	{ "type": FighterClass.Type.Normal,  "stats": {"hp":100, "strength":10, "agility":10, "intelligence":10} },
@@ -18,12 +19,23 @@ const my_chars = [
 	{ "type": FighterClass.Type.Wizard,  "stats": {"hp":100, "strength":10, "agility":10, "intelligence":10} },
 ]
 
+@onready var EndPanel = %EndPanel
+@onready var TitleText = %TitleText
+
+# Private var
+const _FIGHTER_SCENE := preload("res://Fight/Fighter/Fighter.tscn")
+var _i_current_spawn:int = 0
+var _result: Result = Result.Unknown
+
+# Internal callbacks
 func _ready():
+	%EndPanel.hide()
 	$SpawnTimer.start()
 
 func _on_spawn_timer_timeout() -> void:
 	if _i_current_spawn >= my_chars.size() && _i_current_spawn >= op_chars.size():
 		$SpawnTimer.stop()
+		$EndChecker.start()
 		return
 	
 	if _i_current_spawn < my_chars.size():
@@ -35,7 +47,46 @@ func _on_spawn_timer_timeout() -> void:
 	_i_current_spawn = _i_current_spawn + 1
 
 func _spawn_fighter(pos:Vector2, side:FighterClass.Sides, fighter:Variant):
-	var fighter_instance = FIGHTER_SCENE.instantiate() as FighterClass
+	var fighter_instance = _FIGHTER_SCENE.instantiate() as FighterClass
 	fighter_instance.set_characteristic(side, fighter.type)
 	fighter_instance.position = pos
 	$FighterContainer.add_child(fighter_instance)
+
+func _on_button_pressed() -> void:
+	FightEnd.emit(_result)
+
+func _on_end_checker_timeout() -> void:
+	var has_me := false
+	var has_op := false
+
+	for child in get_children():
+		if child is not FighterClass or child.is_queued_for_deletion():
+			continue
+
+		var f := child as FighterClass
+		match f.get_side():
+			FighterClass.Sides.Me: has_me = true
+			FighterClass.Sides.Op: has_op = true
+
+		# early exit: both sides still present => battle continues
+		if has_me and has_op:
+			return
+			
+	# Decide result
+	if not has_me and not has_op: _win()   # no fighters left -> win (as you requested)
+	elif has_me and not has_op: _win()   # only my side left -> win
+	elif has_op and not has_me: _lose()  # only opponent left -> lost		
+
+func _win():
+	_result = Result.Win
+	
+	$EndChecker.stop()
+	%TitleText.set_value("The dungeon is safe")
+	%EndPanel.show()
+	
+func _lose():
+	_result = Result.Lose
+	
+	$EndChecker.stop()
+	%TitleText.set_value("You have failed your duty")
+	%EndPanel.show()
