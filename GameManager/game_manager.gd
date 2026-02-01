57899bfb200f0d6f783417ae1ec715f566e5f96c
Saillou <chauvinremy@upyourprod.fr>
2026-02-01 18:20:22 +0100

    Fix hp

--- GameManager/game_manager.gd
+++ GameManager/game_manager.gd
@@ -31,10 +31,10 @@ var FightScene = preload("res://Fight/Fight.tscn")
 enum ENNEMY_TYPE { Normal, Archer, Warrior, Wizard }
 
 var oponent_dictionnary = {
-	ENNEMY_TYPE.Normal:FighterData.Make(FighterClass.Type.Normal,10,10,10,10),
-	ENNEMY_TYPE.Archer:FighterData.Make(FighterClass.Type.Archer,15,5,20,5),
-	ENNEMY_TYPE.Warrior:FighterData.Make(FighterClass.Type.Warrior,20,20,5,5),
-	ENNEMY_TYPE.Wizard:FighterData.Make(FighterClass.Type.Wizard,10,5,5,25)
+	ENNEMY_TYPE.Normal:FighterData.Make(FighterClass.Type.Normal,50,10,10,10),
+	ENNEMY_TYPE.Archer:FighterData.Make(FighterClass.Type.Archer,100,5,30,5),
+	ENNEMY_TYPE.Warrior:FighterData.Make(FighterClass.Type.Warrior,200,30,5,5),
+	ENNEMY_TYPE.Wizard:FighterData.Make(FighterClass.Type.Wizard,100,5,5,30)
 }
 
 signal state_changed
